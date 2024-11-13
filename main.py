283afda70b8ab510077b9f6e8295ae49e0c9a9f2
Jay <jenjentw358@gmail.com>
2024-11-12 23:13:47 -0500

    feat: avoid using concatenate to make it more efficient.

--- main.py
+++ main.py
@@ -11,9 +11,9 @@ from googletrans import Translator
 
 def main(source, model, translator, file_name = "temp.wav", sample_rate=48000, seconds=10, transcribe_delay=5, update_delay=0.1, translate_to="en"):
     buffer = np.zeros((sample_rate * seconds, 2))
-    update_thread = threading.Thread(target=update_data, args=(source, buffer, sample_rate, update_delay, seconds))
+    update_thread = threading.Thread(target=update_data, args=(source, buffer, sample_rate, update_delay))
     save_thread = threading.Thread(target=save_audio, args=(buffer, sample_rate,file_name))
-    transcribe_thread = threading.Thread(target=transcribe_chunk, args=(model, translator,translate_to, transcribe_delay))
+    transcribe_thread = threading.Thread(target=transcribe_chunk, args=(model, translator, translate_to, transcribe_delay))
 
     update_thread.start()
     save_thread.start()
@@ -30,6 +30,6 @@ google_translator = Translator()
 print("Initialization complete.")
 print()
 try:
-    main(mic, whisper_model, google_translator, file_name="output/temp.wav")
+    main(mic, whisper_model, google_translator, file_name= "output/temp.wav")
 except KeyboardInterrupt:
     print("Exiting...")
--- record.py
+++ record.py
@@ -3,15 +3,14 @@ import numpy as np
 import time
 
 
-def update_data(source, buffer, sample_rate, update_seconds, total_seconds):
+def update_data(source, buffer, sample_rate, update_seconds):
     with source.recorder(samplerate=sample_rate, blocksize=8192) as rec:
         while True:
-            new_data = rec.record(numframes=(sample_rate * update_seconds))
-            updated_buffer = np.concatenate((buffer, new_data), axis=0)
-            buffer[:] = updated_buffer[-(int(sample_rate * total_seconds)):]  # Update buffer in place
+            new_data = rec.record(numframes=int(sample_rate * update_seconds))
+            buffer[:-int(sample_rate * update_seconds)] = buffer[int(sample_rate * update_seconds):]
+            buffer[-int(sample_rate * update_seconds):] = new_data
 
-def save_audio(buffer, sample_rate, target_filename, delay=1):
+def save_audio(buffer, sample_rate, target_filename, delay=0.1):
     while True:
         sf.write(file=target_filename, data=buffer, samplerate=sample_rate)
         time.sleep(delay)
-
