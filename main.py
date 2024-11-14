47e62a6735a59c67f95203a3a0a77199cf4845ea
Jay <jenjentw358@gmail.com>
2024-11-13 20:52:31 -0500

    feat: fix transcriber not transcribing before buffer is full

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ from googletrans import Translator
 def main(source, model, translator, sample_rate=16000, seconds=15, transcribe_delay=5, update_delay=0.1, translate_to="en"):
     buffer = np.zeros(sample_rate * seconds)
     update_thread = threading.Thread(target=update_data, args=(source, buffer, sample_rate, update_delay))
-    transcribe_thread = threading.Thread(target=transcribe_chunk, args=(model,buffer, translator, translate_to, transcribe_delay))
+    transcribe_thread = threading.Thread(target=transcribe_chunk, args=(model, buffer, translator, translate_to, transcribe_delay))
 
     update_thread.start()
     transcribe_thread.start()
@@ -26,6 +26,7 @@ whisper_model = model_init()
 google_translator = Translator()
 print("Initialization complete.")
 print()
+
 try:
     main(mic, whisper_model, google_translator)
 except KeyboardInterrupt:
--- transcribe.py
+++ transcribe.py
@@ -1,11 +1,14 @@
 import time
+import numpy as np
 
-def transcribe_chunk(model,buffer,translator, translate_to,delay):
+def transcribe_chunk(model, buffer, translator, translate_to, delay):
     while True:
         print('\033[1A', end='\x1b[2K')
         print('\033[1A', end='\x1b[2K')
         time.sleep(delay)
-        segments, info = model.transcribe(buffer, beam_size=5)
+        data_end_idx = np.count_nonzero(buffer)
+        data_to_transcribe = buffer[-data_end_idx:]
+        segments, _ = model.transcribe(data_to_transcribe, beam_size=5)
         transcription = " ".join(segment.text for segment in segments)
         print(transcription)
-        print(translator.translate(transcription,dest=translate_to).text)
+        print(translator.translate(transcription, dest=translate_to).text)
